daeb195101a410304934d8b38bf5268918232407
Juan David Aguirre Córdoba <35357414+JD1927@users.noreply.github.com>
2022-05-26 23:20:32 -0500

    Add comment styles

--- first-program/first-program.c
+++ first-program/first-program.c
@@ -1,6 +1,12 @@
+/*
+  Author: Juan Aguirre
+  Purpose: This program prints out my name to the screen.
+  Date:
+*/
 #include <stdio.h>
 int main()
 {
+  // This code is displaying my name.
   printf("Hi, my name is JD!");
   return 0;
 }
