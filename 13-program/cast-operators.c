fadf431692593a0aaf36c309ea3d74dd2145818e
Juan David Aguirre Córdoba <35357414+JD1927@users.noreply.github.com>
2022-06-14 21:22:13 -0500

    Add cast operators

---
+++ 13-program/cast-operators.c
@@ -0,0 +1,16 @@
+/*
+  Author: Juan Aguirre
+  Date:
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main(int argc, char *argv[])
+{
+  float val1 = 1.2365;
+  float val2 = 5.2365;
+  printf("%d\n", (int) val1 + (int) val2);
+  return 0;
+}
