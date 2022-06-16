89f581bc17569f6147a44ad90b2dd41c148ead63
Juan David Aguirre Córdoba <35357414+JD1927@users.noreply.github.com>
2022-06-15 21:46:25 -0500

    Add for loop

---
+++ 19-program/for-loop.c
@@ -0,0 +1,25 @@
+/*
+  Author: Juan Aguirre
+  Date:
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main(int argc, char *argv[])
+{
+  unsigned long long sum = 0LL; // Stores the sum of the integers
+  unsigned int count = 0;       // The number of integers to be summed
+
+  // Read the number of integers to be summed
+  printf("Enter the number of integers you want to sum: \n");
+  scanf("%u", &count);
+
+  // Sum integers from 1 to count
+  for (unsigned int i = 1; i <= count; i++) {
+    sum += i;
+  }
+  printf("\nTotal of the first %u numbers is %lu\n", count, sum);
+  return 0;
+}
