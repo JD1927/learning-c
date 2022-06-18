5d34306ca901e7c7c6ee371f5cedc43d19d13035
Juan David Aguirre Córdoba <35357414+JD1927@users.noreply.github.com>
2022-06-18 13:33:21 -0500

    Add arrays

---
+++ 21-program/arrays.c
@@ -0,0 +1,27 @@
+/*
+  Author: Juan Aguirre
+  Date:
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main(int argc, char *argv[])
+{
+  int grades[10];
+  int count = 10;
+  long sum = 0;
+  float average = 0.0f;
+
+  printf("\nEnter the 10 grades: \n");
+
+  for (int i = 0; i < count; i++) {
+    printf("%2u> ", i + 1);
+    scanf("%d", &grades[i]);
+    sum += grades[i];
+  }
+  average = (float) sum / count;
+  printf("\nAverage of the ten grades entered is: %.2f\n", average);
+  return 0;
+}
