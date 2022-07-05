0598d00634670ae0548e8ede18af6a2c6f197f78
Juan Aguirre <j1927aguirre@gmail.com>
2022-07-04 21:37:40 -0500

    Add pointers by value lesson

---
+++ 42-program/pointers-pass-by-value.c
@@ -0,0 +1,39 @@
+/*
+  Author: Juan Aguirre
+  Date:
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <ctype.h>
+#include <stddef.h>
+
+void swap(int x, int y);
+
+int main(int argc, char *argv[]) {
+  int a = 100;
+  int b = 200;
+
+  printf("Before swap, value of a: %d\n", a);
+  printf("Before swap, value of b: %d\n", b);
+
+  // Calling a function to swap the values
+  swap(a, b);
+
+  printf("After swap, value of a: %d\n", a);
+  printf("After swap, value of b: %d\n", b);
+
+  return 0;
+}
+
+void swap(int x, int y) {
+  int temp;
+
+  temp = x;
+  x = y;
+  y = temp;
+
+  return;
+}
